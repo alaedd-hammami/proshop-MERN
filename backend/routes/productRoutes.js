cc741934e4feb7959c5ff16900f9697cada0fe8e
unknown <alaedd@gmail.com>
2021-06-14 00:43:49 +0100

    Prepare for deployment

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import {
+  getProductById,
+  getProducts,
+  deleteProduct,
+  updateProduct,
+  createProduct,
+  createProductReview,
+  getTopProducts,
+} from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+
+// router.get('/', getProducts) : same as below
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
+
+export default router
